a7d2d9ea483f9b94d6f37392e4d4ce06bcb4fe0b
leonidlisneskiy <leonidlisnevskiy@gmail.com>
2017-05-03 23:39:58 +0300

    fix header/logo/etc

--- src/js/app.js
+++ src/js/app.js
@@ -23,44 +23,56 @@ function video_frame(){
 $(document).ready(function() {
 
 		//animate scroll
-	$('.js-scroll-to').on("click", function(e){
-		e.preventDefault();
-		var anchor = $(this);
-		$('html, body').stop().animate({
-			scrollTop: $(anchor.attr('href')).offset().top
-		}, 1000);
-	});
+		$('.js-scroll-to').on("click", function(e){
+			e.preventDefault();
+			var anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top
+			}, 1000);
+		});
 
-	$('.navbar').affix({
-		offset: {
-			top: $('.navbar ').height()
-		}
-	});
-
-	var $sliderReview = $(".js-slider-review");
-
-	$sliderReview.slick({
-		slidesToShow: 1,
-		autoplay: true,
-		autoplaySpeed: 5000,
-		draggable: false,
-		dots: true,
-		arrows: true,
-		responsive: [
-		{
-			breakpoint: 768,
-			settings: {
-				dots: true
+		$('.navbar').affix({
+			offset: {
+				top: $('.navbar ').height()
 			}
-		},
-		{
-			breakpoint: 568,
-			settings: {
-				arrows: false,
-				dots: true
+		});
+
+		var $sliderReview = $(".js-slider-review");
+		var $sliderFix = $(".js-slider-fix");
+
+		$sliderFix.slick({
+			responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					dots: false
+				}
 			}
-		}
-		]
+			]
+		});
+
+		$sliderReview.slick({
+			slidesToShow: 1,
+			autoplay: true,
+			autoplaySpeed: 5000,
+			draggable: false,
+			dots: true,
+			arrows: true,
+			responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					dots: true
+				}
+			},
+			{
+				breakpoint: 568,
+				settings: {
+					arrows: false,
+					dots: true
+				}
+			}
+			]
+		});
 	});
-});
 
